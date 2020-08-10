64edf013bb3337d1ead1e612d75bdcff15e0476c
Gamer_Z <github@gz0.nl>
2020-08-10 19:57:06 +0200

    Initial Commit

---
+++ RandomSpawn.cs
@@ -0,0 +1,87 @@
+﻿using Pipliz.JSON;
+using System.Collections.Generic;
+using System.IO;
+using TerrainGeneration;
+using Pipliz;
+
+namespace grasmanek94.RandomSpawn
+{
+    [ModLoader.ModManager]
+    public static class RandomSpawn
+    {
+        public static string config_file;
+
+        [ModLoader.ModCallback(ModLoader.EModCallbackType.AfterWorldLoad, "grasmanek94.RandomSpawn.AfterWorldLoad")]
+        static void AfterWorldLoad()
+        {
+            config_file = Path.Combine("gamedata", "savegames", ServerManager.WorldName, "random_spawn_config.json");
+
+            TerrainGenerator terrainGenerator = ServerManager.TerrainGenerator as TerrainGenerator;
+            RandomSpawnProvider spawn_provider = new RandomSpawnProvider();
+
+            bool not_exists = !File.Exists(config_file);
+            if (not_exists)
+            {
+                JSON.Serialize(config_file, JSON.DeserializeString("{\"coarsness\": 10,\"ranges\": [{\"min\": [-100, -100],\"max\": [100, 100]},{\"min\": [-1000, -1000],\"max\": [-900, -900]}],\"spawnpoints\": [[0, 0],[10, 10],[-10,-10]]}"));
+            }
+
+            var data = JSON.Deserialize(config_file, false);
+
+            data.TryGetAsOrDefault("coarsness", out spawn_provider.coarsness, 10);
+            if (spawn_provider.coarsness < 1)
+            {
+                Log.WriteWarning("RandomSpawn: 'coarsness' must be at least 1, it's currently set to '{0}', setting to '1' automatically.", spawn_provider.coarsness);
+                spawn_provider.coarsness = 1;
+            }
+
+            JSONNode ranges;
+            if (data.TryGetChild("ranges", out ranges))
+            {
+                foreach (var range in ranges.LoopArray())
+                {
+                    JSONNode range_min;
+                    JSONNode range_max;
+
+                    if (range.TryGetAs("min", out range_min) && range.TryGetAs("max", out range_max))
+                    {
+                        Vector2Int a = new Vector2Int(range_min[0].GetAs<int>(), range_min[1].GetAs<int>());
+                        Vector2Int b = new Vector2Int(range_max[0].GetAs<int>(), range_max[1].GetAs<int>());
+
+                        spawn_provider.ranges.Add(
+                            new KeyValuePair<Vector2Int, Vector2Int>(
+                                new Vector2Int(Math.Min(a.x, b.x), Math.Min(a.y, b.y)),
+                                new Vector2Int(Math.Max(a.x, b.x), Math.Max(a.y, b.y))
+                            )
+                        );
+                    }
+                }
+            }
+
+            JSONNode spawnpoints;
+            if (data.TryGetChild("spawnpoints", out spawnpoints))
+            {
+                foreach (var spawnpoint in spawnpoints.LoopArray())
+                {
+                    if (spawnpoint.NodeType == NodeType.Array && spawnpoint.ChildCount == 2)
+                    {
+                        spawn_provider.custom_spawnpoints.Add(new Vector2Int(spawnpoint[0].GetAs<int>(), spawnpoint[1].GetAs<int>()));
+                    }
+                }
+            }
+
+            if (not_exists)
+            {
+                Log.WriteWarning("RandomSpawn: File \"{0}\" doesn't exist and has been created. Using defaults. Please edit this file.", config_file);
+            }
+
+            terrainGenerator.SpawnPointProvider = spawn_provider;
+        }
+
+        [ModLoader.ModCallback(ModLoader.EModCallbackType.OnPlayerRespawn, "grasmanek94.RandomSpawn.OnPlayerRespawn")]
+        static void OnPlayerRespawn(Players.Player player)
+        {
+            ServerManager.WorldSettingsVariable.SpawnPosition = (ServerManager.TerrainGenerator as TerrainGenerator).SpawnPointProvider.GetSpawnPoint();
+        }
+        
+    }
+}
---
+++ RandomSpawnProvider.cs
@@ -0,0 +1,76 @@
+﻿using Pipliz;
+using System.Collections.Generic;
+using TerrainGeneration;
+using static TerrainGeneration.TerrainGenerator;
+
+namespace grasmanek94.RandomSpawn
+{
+    public class RandomSpawnProvider : ISpawnPointProvider
+    {
+        public static readonly int max_retries = 50;
+        public int coarsness;
+        public List<KeyValuePair<Vector2Int, Vector2Int>> ranges;
+        public List<Vector2Int> custom_spawnpoints;
+
+        public RandomSpawnProvider()
+        {
+            coarsness = 10;
+            ranges = new List<KeyValuePair<Vector2Int, Vector2Int>>();
+            custom_spawnpoints = new List<Vector2Int>();
+        }
+
+        public Vector3Int GetSpawnPoint()
+        {
+            TerrainGenerator terrainGenerator = ServerManager.TerrainGenerator as TerrainGenerator;
+            int waterLevel = terrainGenerator.GeneratorSettings.BaseSettings.WaterLevel;
+
+            bool use_range = ranges.Count > 0;
+            bool use_custom = custom_spawnpoints.Count > 0;
+            bool use_both = use_range && use_custom;
+
+            if (use_both)
+            {
+                if (Random.Next() % 2 == 0)
+                {
+                    use_range = false;
+                }
+                else
+                {
+                    use_custom = false;
+                }
+            }
+
+            if (use_range)
+            {
+                for (int i = 0; i < max_retries; ++i)
+                {
+                    KeyValuePair<Vector2Int, Vector2Int> range = ranges[Random.Next() % ranges.Count];
+
+                    Vector2Int delta = (range.Value - range.Key) / coarsness;
+                    Vector2Int rand = new Vector2Int(Random.Next() % coarsness, Random.Next() % coarsness);
+                    Vector2Int result = range.Key + new Vector2Int(delta.x * rand.x, delta.y * rand.y);
+
+                    int num = (int)terrainGenerator.QueryData(result.x, result.y).Height;
+                    if (num > waterLevel)
+                    {
+                        //Log.WriteWarning("GetSpawnPoint::use_range({0}, {1}, {2})", result.x, num + 1, result.y);
+                        return new Vector3Int(result.x, num + 1, result.y);
+                    }
+                }
+
+                use_custom = custom_spawnpoints.Count > 0;
+            }
+
+            if (use_custom)
+            {
+                Vector2Int result = custom_spawnpoints[Random.Next() % custom_spawnpoints.Count];
+                int num = (int)terrainGenerator.QueryData(result.x, result.y).Height;
+                //Log.WriteWarning("GetSpawnPoint::use_custom({0}, {1}, {2})", result.x, num + 1, result.y);
+                return new Vector3Int(result.x, num + 1, result.y);
+            }
+
+            //Log.WriteWarning("GetSpawnPoint::default()");
+            return new Vector3Int(0, (int)terrainGenerator.QueryData(0, 0).Height + 1, 0);
+        }
+    }
+}
